c4dc7b08d05b343039b9ca08aa976c1c1ad67c62
ab2student <81992115+ab2student@users.noreply.github.com>
2021-05-29 11:33:49 +0000

    Add files via upload

---
+++ js/profile-client.js
@@ -0,0 +1,53 @@
+/**** RESPONSIVE NAVBAR ****/
+
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+/***** PHOTO DE PROFILE *****/
+
+const imgDiv = document.querySelector('.input-group-photo');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+//if user hover on img div 
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+//if we hover out from img div
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+//lets work for image showing functionality when we choose an image to upload
+
+//when we choose a photo to upload
+
+file.addEventListener('change', function(){
+    //this refers to file
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader(); //FileReader is a predefined function of JS
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+    }
+});
--- js/script.js
+++ js/script.js
@@ -1,1 +1,104 @@
+/*
+const slide = document.querySelector('.slide');
+const slideImages = document.querySelectorAll('.slide img');
 
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = slideImages[0].clientWidth;
+
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Button listeners
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= slideImages.length - 1) return;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+slide.addEventListener('transitionend', () =>  {
+    if(slideImages[counter].id === 'lastClone'){
+        slide.style.transition = "none";
+        counter = slideImages.length -2;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }else{
+            if(slideImages[counter].id === 'firstClone'){
+                slide.style.transition = "none";
+                counter = slideImages.length - counter;
+                slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+            }
+        }
+});
+*/
+/******************** JS Etoiles d'évaluations ******************************/
+
+
+const allstars = document.querySelectorAll(".stars");
+const rating = document.querySelector('.rating');
+
+
+init();
+
+function init(){
+    allstars.forEach((star) => {
+        star.addEventListener("click", saveRating);
+        star.addEventListener("mouseover", addCSS); //checked dans CSS
+        star.addEventListener("mouseleave", removeCSS); 
+
+    });
+}
+
+function saveRating(e){
+    removeEventAllStars();
+    rating.innerText = e.target.dataset.star;
+}
+
+function removeEventAllStars(){
+    allstars.forEach(star => {
+        star.removeEventListener("click", saveRating);
+        star.removeEventListener("mouseover", addCSS);
+        star.removeEventListener("mouseleave", removeCSS);
+
+    });
+}
+
+function addCSS(e, css="checked") {
+    const overedStar = e.target; 
+    overedStar.classList.add(css);
+    const previousSiblings = getPreviousSiblings(overedStar);
+    previousSiblings.forEach(elem => elem.classList.add(css));
+    console.log("elem.previousSiblings", previousSiblings);
+    
+}
+
+function removeCSS(e, css="checked"){
+    const overedStar = e.target;
+    e.target.classList.remove(css);
+    const previousSiblings = getPreviousSiblings(overedStar);
+    previousSiblings.forEach(elem => elem.classList.remove(css));
+
+}
+
+function getPreviousSiblings(elem) {
+    console.log("elem.previousSibling", elem.previousSibling);
+    let siblings = [];
+    const spanNodeType = 3; //1 c'est le type des spans 
+    while(elem = elem.previousSibling){
+        if(elem.nodeName === "SPAN"){
+            siblings = [elem, ...siblings];
+        }
+    }
+    return siblings;
+}
